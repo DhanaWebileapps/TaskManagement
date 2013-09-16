6701fca20f5099a01aaa9ab2c080f865c5a7e8b9
Dhana Gadupooti <dhana@webileapps.com>
2013-09-16 18:13:37 +0530

    Initial Commit

---
+++ TaskManagement/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TaskManagement
+//
+//  Created by Dhana Gadupooti on 16/09/13.
+//  Copyright (c) 2013 Dhana Gadupooti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
